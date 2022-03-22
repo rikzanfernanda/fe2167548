95377b39294dbc47aa4f0bb6a186e027434e3231
rikzan fernanda <fernandarikzan@gmail.com>
2022-03-22 16:47:54 +0700

    chore: answered

--- frontend/javascript/3-comparison-cp/script-1.js
+++ frontend/javascript/3-comparison-cp/script-1.js
@@ -3,4 +3,14 @@
 // Masukan suatu angka
 const number = parseInt(prompt("Masukan bilangan: "));
 
+// cek
+// let number = -1;
+
 // TODO: answer here
+if (number > 0) {
+    console.log("bilangan positif");
+} else if (number == 0) {
+    console.log("bilangan = 0");
+} else {
+    console.log("bilangan negatif");
+}
