1f3eda3730163015f02772239e521a9dabdddf29
rikzan fernanda <fernandarikzan@gmail.com>
2022-03-22 17:54:13 +0700

    chore: answered

--- frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-2.js
+++ frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-2.js
@@ -2,3 +2,14 @@
 // Seseorang diperbolehkan membuat SIM jika usianya lebih besar atau sama dengan 17 tahun.
 
 // TODO: answer here
+/*
+START
+DISPLAY "Enter the age: "
+READ age
+if age >= 17 THEN
+	DISPLAY "Boleh membuat SIM"
+ELSE
+	DISPLAY "Tidak boleh membuat SIM"
+END IF
+STOP
+*/
--- frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-3.js
+++ frontend/javascript/1-algorithm-and-pseudocode-cp/exercise-3.js
@@ -1,3 +1,16 @@
 // Buatlah pseudocode untuk menjumlahkan bilangan genap yang ada di antara 1 sampai 10
 
 // TODO: answer here
+/*
+VAR n_genap: integer
+
+START
+n_genap := 0;
+FOR i FROM 1 -> 10:
+	IF i % 2 == 0 THEN
+		n_genap += i
+ENDLOOP
+
+DISPLAY n_genap
+STOP
+*/
