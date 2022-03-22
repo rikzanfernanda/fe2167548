2ba8336e7a5724300f9aca37873b28e522976c99
rikzan fernanda <fernandarikzan@gmail.com>
2022-03-22 16:28:36 +0700

    chore: answered

--- frontend/javascript/2-variable-cp/script.js
+++ frontend/javascript/2-variable-cp/script.js
@@ -4,6 +4,11 @@ let a = prompt('Masukan variabel pertama: ');
 let b = prompt('Masukan variabel kedua: ');
 
 // TODO: answer here
+let temp;
+
+temp = a;
+a = b;
+b = temp;
 
 console.log(`Nilai a setelah ditukar: ${a}`);
 console.log(`Nilai b setelah ditukar: ${b}`);
