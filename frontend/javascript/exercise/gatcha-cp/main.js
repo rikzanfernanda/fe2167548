9008f0df67be2b88bad65814c1665d391aba42b2
rikzan fernanda <fernandarikzan@gmail.com>
2022-03-28 23:17:03 +0700

    chore: answered

--- frontend/javascript/exercise/gatcha-cp/main.js
+++ frontend/javascript/exercise/gatcha-cp/main.js
@@ -10,14 +10,44 @@
  */
 
 // PSEUDOCODE:
-// TODO: answer here
+/*
+FUNCTION gatcha(button):
+  SWITCH button:
+    CASE 1:
+      RETURN "coba lagi ya"
+    CASE 2:
+      RETURN "selamat kamu mendapatkan kupon sebanyak 5"
+    CASE 3:
+      RETURN "selamat kamu mendapatkan kupon sebanyak 15"
+    CASE 4:
+      RETURN "selamat kamu mendapatkan kupon sebanyak 50"
+    CASE 5:
+      RETURN "selamat kamu mendapatkan kupon sebanyak 100"
+    DEFAULT:
+      RETURN "coba lagi ya"
+  END SWITCH
+END FUNCTION
+*/
 
 function gatcha(button) {
-  // TODO: answer here
+  switch (button) {
+    case 1:
+      return 'coba lagi ya';
+    case 2:
+      return 'selamat kamu mendapatkan kupon sebanyak 5';
+    case 3:
+      return 'selamat kamu mendapatkan kupon sebanyak 15';
+    case 4:
+      return 'selamat kamu mendapatkan kupon sebanyak 50';
+    case 5:
+      return 'selamat kamu mendapatkan kupon sebanyak 100';
+    default:
+      return 'coba lagi ya';
+  }
 }
 
 // Create variable button here
-// TODO: answer here
+let button = Math.floor(Math.random() * 5) + 1;
 
 console.log(gatcha(button))
 
