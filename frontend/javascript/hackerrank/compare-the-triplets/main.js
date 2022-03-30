4e7804c548903e40ce58880587fe3f89b36440b3
rikzan fernanda <fernandarikzan@gmail.com>
2022-03-31 06:24:48 +0700

    chore: answered

--- frontend/javascript/hackerrank/compare-the-triplets/main.js
+++ frontend/javascript/hackerrank/compare-the-triplets/main.js
@@ -15,8 +15,17 @@
 // Full Problem: https://www.hackerrank.com/challenges/compare-the-triplets/problem
 
 function compareTriplets(a, b) {
-  // Write your code here
-  // TODO: answer here
+  const result = [0, 0]
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > b[i]) {
+      result[0]++
+    } else if (a[i] < b[i]) {
+      result[1]++
+    }
+  }
+
+  return result
 }
 function main() {
 
