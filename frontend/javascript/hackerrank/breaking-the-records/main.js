a2f21e5af112ce58915b57d87eb38c6c920e0b16
rikzan fernanda <fernandarikzan@gmail.com>
2022-03-31 05:56:44 +0700

    chore: answered

--- frontend/javascript/hackerrank/breaking-the-records/main.js
+++ frontend/javascript/hackerrank/breaking-the-records/main.js
@@ -13,8 +13,22 @@
 // Full Problem: https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
 
 function breakingRecords(scores) {
-  // Write your code here
-  // TODO: answer here
+  let highest = scores[0];
+  let lowest = scores[0];
+  let highestCount = 0;
+  let lowestCount = 0;
+
+  for (let i = 0; i < scores.length; i++) {
+    if (scores[i] > highest) {
+      highest = scores[i];
+      highestCount++;
+    } else if (scores[i] < lowest) {
+      lowest = scores[i];
+      lowestCount++;
+    }
+  }
+
+  return [highestCount, lowestCount];
 }
 
 function main() {
@@ -24,6 +38,7 @@ function main() {
   var scores = [10, 5, 20, 20, 4, 5, 2, 25, 1]; // override input
   var result = breakingRecords(scores);
   console.log(result.join(' ') + '\n')
+  console.log(breakingRecords([3, 4, 21, 36, 10, 28, 35, 5, 24, 42]));
 
 }
 
