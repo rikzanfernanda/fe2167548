1529227cbecf56f3c7e161ef8a5cde8078775ce6
rikzan fernanda <fernandarikzan@gmail.com>
2022-03-31 11:59:45 +0700

    chore: answer

--- frontend/javascript/study-case/study-case-1/study-case-1.js
+++ frontend/javascript/study-case/study-case-1/study-case-1.js
@@ -23,9 +23,22 @@
 
 
 function rotation(matrix) {
-    // TODO: answer here
+    // for (let i = 0; i < matrix.length; i++) {
+    //     for (let j = 0; j < i; j++) {
+    //         [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    //     }
+    // }
+    // for (let i = 0; i < matrix.length; i++) {
+    //     matrix[i].reverse();
+    // }
+    // return matrix;
+    
+    for (let i = 0; i < matrix.length; i++){
+        
+    }
+
 };
 
-// TODO: answer here
+console.log(rotation([[1,2,3],[4,5,6],[7,8,9]]));
 
 module.exports = rotation
