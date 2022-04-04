ec620b33d3f87059ee09366d86a1700b0b16865c
rikzan fernanda <fernandarikzan@gmail.com>
2022-04-04 15:13:43 +0700

    chore: solved for calculator

--- frontend/dom/calculator-cp/index.js
+++ frontend/dom/calculator-cp/index.js
@@ -1,14 +1,32 @@
+const input = document.getElementById('input');
+
 //handle when the number is pressed. It renders the number into #input element
 function number(number) {
-    // TODO: answer here
+    input.value += number;
 }
 
 //handle when the Del button, AC button and other operators (+, /, -, x) are pressed. It renders the operator into #input element
 function operator(operator) {
-    // TODO: answer here
+    switch (operator) {
+        case 'AC': {
+            input.value = '';
+            break;
+        }
+        case 'Del': {
+            input.value = input.value.slice(0, -1);
+            break;
+        }
+        default: {
+            input.value += operator;
+        }
+    }
 }
 
 //handle when = button is pressed. It renders the result into #input element
 function calculate() {
-    // TODO: answer here
+    try {
+        input.value = eval(input.value);
+    } catch (e) {
+        alert('Invalid input');
+    }
 }
