cfb45e40481d1d32dd1bd6da915ae53656ef7cfe
rikzan fernanda <fernandarikzan@gmail.com>
2022-04-05 06:31:28 +0700

    chore: solved for guess number

--- frontend/dom/guess-number-assignment/index.js
+++ frontend/dom/guess-number-assignment/index.js
@@ -2,29 +2,70 @@
 Uncomment variable dibawah ini untuk mulai mengerjakan. dilarang mengganti nama variable yang dibuat.
 */
 
-// let newGameBtn = tombol untuk melakukan restart game
-// let randomNumber = generate random number dari 1 sampai 10
-// let message = element untuk menampilkan pesan
-// let displayScore = element untuk menampilkan score
-// let input = element untuk memasukan data
-// let checkBtn = tombol untuk melakuan pengecekan angka pada input
-// let score = nilai yang akan ditampilakan
+let newGameBtn = document.querySelector('.new-game');
+let randomNumber = document.querySelector('#hidden-number');
+let message = document.querySelector('.message');
+let displayScore = document.querySelector('.score');
+let input = document.querySelector('.input');
+let checkBtn = document.querySelector('button.check');
+let score = document.querySelector('.score');
+let number = document.querySelector('.number');
 
-// TODO: answer here
+randomNumber = generateNumber();
+score = 10;
 
+newGameBtn.addEventListener('click', () => {
+  resetGame();
+});
+
+checkBtn.addEventListener('click', () => {
+  checkNumber();
+});
+
+function generateNumber() {
+  return Math.floor(Math.random() * 10 + 1);
+}
 
 function displayMessage(msg) {
-  // TODO: answer here
+  message.innerHTML = msg;
 }
 
 function resetGame() {
-  // TODO: answer here
+  message.innerHTML = 'Lets start guessing...';
+  randomNumber = generateNumber();
+  score = 10;
+  displayScore.innerHTML = score;
+
   //dilarang menghapus code dibawah ini!
   document.getElementById('hidden-number').innerHTML = randomNumber
 }
 
 function checkNumber() {
-  // TODO: answer here
+  number.innerHTML = '?';
+  // randomNumber = generateNumber();
+
+  if (parseInt(input.value) >= 1 && parseInt(input.value) <= 10) {
+    if (parseInt(input.value) === randomNumber) {
+      displayMessage('Yeay! you guessed it!');
+      number.innerHTML = randomNumber;
+    } else {
+      score--;
+  
+      if (score < 1) {
+        displayMessage('Oops, you lost the game');
+      } else if (parseInt(input.value) < randomNumber) {
+        displayMessage('Too small, buddy!');
+      } else if (parseInt(input.value) > randomNumber) {
+        displayMessage("Oww... that's too big!");
+      }
+    }
+  } else {
+    displayMessage('Guess any number between 1 and 10');
+  }
+
+  displayScore.innerHTML = score;
+  randomNumber = generateNumber();
+  document.getElementById('hidden-number').innerHTML = randomNumber;
 }
 
 //dilarang menghapus code dibawah ini!
