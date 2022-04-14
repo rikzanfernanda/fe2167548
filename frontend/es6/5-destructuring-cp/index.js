94729c41b81c03f46dd1b7872b94b33757ebfe51
rikzan fernanda <fernandarikzan@gmail.com>
2022-04-14 12:04:45 +0700

    chore: answered

--- frontend/es6/5-destructuring-cp/index.js
+++ frontend/es6/5-destructuring-cp/index.js
@@ -1,7 +1,8 @@
 // format nama user menjadi "John - john@example.com", dan akses nama user dengan teknik destrucuring assignment.
 
 const format = (user) => {
-  // TODO: answer here
+  const {name, email} = user;
+  return `${name} - ${email}`;
 };
 
 console.log(format({ name: "John", email: "john@example.com" }))
