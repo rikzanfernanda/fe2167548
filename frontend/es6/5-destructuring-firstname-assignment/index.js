16cdca8ebd62f093a03f622f537e3f3da03a4f15
rikzan fernanda <fernandarikzan@gmail.com>
2022-04-16 13:13:01 +0700

    chore: answered

--- frontend/es6/5-destructuring-firstname-assignment/index.js
+++ frontend/es6/5-destructuring-firstname-assignment/index.js
@@ -1,7 +1,8 @@
 /** Akses user name, namun dengan nama variabel yang berbeda dari atribut pada objek user
  */
 const getUserFirstName = (user) => {
-  // TODO: answer here
+  const { name: firstName} = user;
+  return firstName;
 };
 
 console.log(getUserFirstName({ name: "John", email: "john@example.com" }));
