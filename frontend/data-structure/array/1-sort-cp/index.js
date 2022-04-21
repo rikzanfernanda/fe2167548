08a391337ef2975422544469fd76f2aabe8ce5f0
rikzan fernanda <fernandarikzan@gmail.com>
2022-04-21 17:33:44 +0700

    chore: answered

--- frontend/data-structure/array/1-sort-cp/index.js
+++ frontend/data-structure/array/1-sort-cp/index.js
@@ -13,11 +13,27 @@
 // [1,2,3,4,5] -> [2,3,4,5,1] -> [3,4,5,1,2] -> [4,5,1,2,3] -> [5,1,2,3,4]
 
 function sort(arr) {
-    return arr // TODO: replace this
+    for (let i = 0; i < arr.length-1; i++) {
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                let tmp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = tmp;
+            }
+        }
+    }
+    return arr;
 }
 
 function rotateLeft(d, arr) {
-    return arr // TODO: replace this
+    for (let i = 0; i < d; i++) {
+        let tmp = arr[0];
+        for (let j = 0; j < arr.length-1; j++) {
+            arr[j] = arr[j+1];
+        }
+        arr[arr.length-1] = tmp;
+    }
+    return arr;
 }
 
 let nums = [4, 5, 2, 1, 3]
