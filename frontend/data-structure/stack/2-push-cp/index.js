3ad8b5a7eddba2d4951e9f236bd804616dd15cca
rikzan fernanda <fernandarikzan@gmail.com>
2022-04-22 15:06:10 +0700

    chore: answered

--- frontend/data-structure/stack/2-push-cp/index.js
+++ frontend/data-structure/stack/2-push-cp/index.js
@@ -2,10 +2,17 @@
 
 module.exports = class Stack {
     constructor() {
-        // TODO: answer here
+        this.data = [];
+        this.top = -1;
+        this.size = 10;
     }
 
     push(elemen) {
-        // TODO: answer here
+        if (this.top < this.size - 1) {
+            this.top++;
+            return this.data.push(elemen);
+        } else {
+            throw new Error('stack overflow');
+        }
     }
 }
